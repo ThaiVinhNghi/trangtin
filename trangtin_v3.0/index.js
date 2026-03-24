6f00082b3783b7ed108331d2c04289d58bfa9469
ThaiVinhNghi <vinhnghithai1207@gmail.com>
2026-03-24 15:04:24 +0700

    fix css

--- trangtin_v3.0/index.js
+++ trangtin_v3.0/index.js
@@ -15,40 +15,38 @@ mongoose.connect(uri)
 .then(() => console.log('Đã kết nối thành công tới MongoDB.'))
 .catch(err => console.log(err));
 
- 
 app.set('views', './views');
 app.set('view engine', 'ejs');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/', express.static(path.join(__dirname, 'public')));
+
+// ✅ CHỈ GIỮ 1 DÒNG NÀY
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-	name: 'iNews',						// Tên session (tự chọn)
-	secret: 'Mèo méo meo mèo meo',		// Khóa bảo vệ (tự chọn)
+	name: 'iNews',
+	secret: 'Mèo méo meo mèo meo',
 	resave: false,
 	saveUninitialized: false,
 	cookie: {
-		maxAge: 30 * 24 * 60 * 60 * 1000// Hết hạn sau 30 ngày
+		maxAge: 30 * 24 * 60 * 60 * 1000
 	}
 }));
 
 app.use((req, res, next) => {
-	// Chuyển biến session thành biến cục bộ
 	res.locals.session = req.session;
-	
-	// Lấy thông báo (lỗi, thành công) của trang trước đó (nếu có)
+
 	var err = req.session.error;
 	var msg = req.session.success;
-	
-	// Xóa session sau khi đã truyền qua biến trung gian
+
 	delete req.session.error;
 	delete req.session.success;
-	
-	// Gán thông báo (lỗi, thành công) vào biến cục bộ
+
 	res.locals.message = '';
 	if (err) res.locals.message = '<span class="text-danger">' + err + '</span>';
 	if (msg) res.locals.message = '<span class="text-success">' + msg + '</span>';
-	
+
 	next();
 });
 
@@ -57,8 +55,7 @@ app.use('/', authRouter);
 app.use('/chude', chudeRouter);
 app.use('/taikhoan', taikhoanRouter);
 app.use('/baiviet', baivietRouter);
-app.use(express.static('public'));
 
 app.listen(3000, () => {
-	console.log('Server is running at http://127.0.0.1:3000');
+	console.log('Server is running...');
 });
